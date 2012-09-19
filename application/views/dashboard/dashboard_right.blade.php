e907ba58f42028854820af0371c3b1854ca4d260
Anthony Bovasso <bovasso@gmail.com>
2012-09-18 23:58:29 -0400

    added dashboard for teachers.
    Signed-off-by: Anthony Bovasso <bovasso@gmail.com>

---
+++ application/views/dashboard/dashboard_right.blade.php
@@ -0,0 +1,151 @@
+<h1>ASSIGNMENTS</h1>
+<div id="assignment-tabs" class="tabs">
+	<ul>
+		<li class="assignment-tab-1"><a href="#assignment-tabs-1">SETUP</a></li>
+		<li class="assignment-tab-2"><a href="#assignment-tabs-2">WEEK 1</a></li>
+		<li class="assignment-tab-3"><a href="#assignment-tabs-3">WEEK 2</a></li>
+		<li class="assignment-tab-4"><a href="#assignment-tabs-4">WEEK 3</a></li>
+		<li class="assignment-tab-5"><a href="#assignment-tabs-5">WEEK 4</a></li>
+		<li class="assignment-tab-6"><a href="#assignment-tabs-6">WEEK 5</a></li>
+		<li class="assignment-tab-7"><a href="#assignment-tabs-7">FINAL PROJECT</a></li>
+	</ul>
+	<div id="assignment-tabs-1" class="tab">
+		<div class="tab-inner">
+
+			<div class="w100 left">					
+				<div class="left avatar a55 w10">
+					<span class="airplane-success"></span>
+				</div><!-- end Avatar -->
+				<div class="comment-success right w90">
+					<p class="heavy">Welcome to PenPal News!</p>
+					<p>What next?</p>
+				</div>	
+			</div>
+
+			<ol id="setup_steps">
+				<li>
+					<p class="heavy">Give the group codes below to your students.  Direct them to enter the codes at 
+<a href="#">www.penpalnews.com</a></p>
+					
+					<table border="0" cellspacing="5" cellpadding="5">
+    					<tr>
+    						<th>CLASS</th>
+    						<th>GROUP CODE</th>
+    					</tr>
+    					<tr>
+    						<td>Class 1</td>
+    						<td>XYNve67</td>
+    					</tr>
+    					 <tr>
+    						<td>Class 1</td>
+    						<td>XYNve67</td>
+    					</tr>
+    					<tr>
+    						<td>Class 1</td>
+    						<td>XYNve67</td>
+    					</tr>
+    					 <tr>
+    						<td>Class 1</td>
+    						<td>XYNve67</td>
+    					</tr>
+					</table>
+
+					<div class="alert">
+						<p class="heavy">Important: Deadline for Student Registration: Thursday, September 20</p>
+						<p>Student that miss the deadline won't be matched with their own PenPal</p>
+					</div>
+
+				</li>
+				<li>
+					<p class="heavy">We'll send you an email introducing you to your partner class(es)</p>
+				</li>
+				<li>
+					<p class="heavy">Your exchange will begin Tuesday, September 25.</p>
+				</li>
+			</ol>
+			<!-- end Setup Steps -->
+			<p class="heavy">Questions? Check out our <a href="#">FAQs</a></p>
+
+		</div>
+	</div><!-- end Assignment Tab 1 -->
+
+	<div id="assignment-tabs-2" class="tab">
+		<div class="tab-inner">
+				<div class="due-date right">Due Monday, October 2, 2012</div>
+				<h2>YOUR STUDENTS' TO-DO LIST</h2>
+				<h3 class="heavy">1. Watch the Video<br />Immigration Explained in One Minute</h3>
+				<div class="featured-topic w100">
+					<div class="img-wrapper left "> 
+						<img src="http://placehold.it/272x153" class="">
+					</div>
+					<div class="right">
+						<a href="#">The Economy Explained in One Minute</a>
+						<p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p> 
+						<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue.</p>
+					</div>
+				</div><!-- end Featured Assignment Topic -->
+
+				<div class="w100">
+					<h3 class="heavy">2. Select a News Story about the Economy</h3>
+					<p class="instructions">Students will select news to reas and write about or you may direct the whole class to read an article of your choice. Students should answer the questions associated with the story.</p>
+					<ul class="assignments-list">
+						<li>
+							<div class="left w20">
+								<img src="http://placehold.it/173x96" class="">	
+							</div>
+							<div class="right w70">
+								<a href="#">The Economy Explained in One Minute</a>
+								<p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p> 
+								<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue.</p>
+								<p class="read-more"><a href="#">Read More...</a></p>
+							</div><!-- end Featured Assignment Topic -->
+						</li>
+						<li>
+							<div class="left w20">
+								<img src="http://placehold.it/173x96" class="">	
+							</div>
+							<div class="right w70">
+								<a href="#">The Economy Explained in One Minute</a>
+								<p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p> 
+								<p>Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue.</p>
+								<p class="read-more"><a href="#">Read More...</a></p>
+							</div><!-- end Featured Assignment Topic -->
+						</li>
+					</ul>
+
+				</div>
+
+		</div>
+	</div><!-- end Assignment Tab 2 -->
+
+	<div id="assignment-tabs-3" class="tab">
+		<div class="tab-inner">
+
+		</div>
+	</div><!-- end Assignment Tab 3 -->
+
+	<div id="assignment-tabs-4" class="tab">
+		<div class="tab-inner">
+
+		</div>
+	</div><!-- end Assignment Tab 4 -->
+
+	<div id="assignment-tabs-5" class="tab">
+		<div class="tab-inner">
+
+		</div>
+	</div><!-- end Assignment Tab 5 -->
+
+	<div id="assignment-tabs-6" class="tab">
+		<div class="tab-inner">
+
+		</div>
+	</div><!-- end Assignment Tab 6 -->
+
+	<div id="assignment-tabs-7" class="tab">
+		<div class="tab-inner">
+
+		</div>
+	</div><!-- end Assignment Tab 7 -->
+
+</div>
--- application/views/dashboard/index.blade.php
+++ application/views/dashboard/index.blade.php
@@ -1,7 +1,17 @@
 @layout('layouts/main')
 
 @section('content')
+	
+	<div id="dashboard" class="wrapper">
 
+		<div id="dashboard_left" class="left w30">
+			@include('dashboard/dashboard_left')
+		</div>
 
+		<div id="dashboard_right" class="right w70">
+			@include('dashboard/dashboard_right')
+		</div>
+
+	</div>
 
 @endsection
